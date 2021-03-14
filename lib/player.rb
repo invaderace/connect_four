73cc955f26afec2e151548d9ca9bc082ee41207e
Michael Mattson <michael.p.mattson@gmail.com>
2021-03-14 16:50:20 -0600

    added make_disc

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
-#
+require_relative 'disc.rb'
+
+# player has name and color. performs various actions on discs.
 class Player
-  attr_accessor :name, color
+  attr_accessor :name, :color
 
   def initialize(name, color)
     @name = name
     @color = color
+  end
+
+  # use player's color to make new disc object.
+  def make_disc
+    Disc.new(@color)
   end
 end
--- spec/player.rb
+++ spec/player.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
-require_relative '../lib/player'
+require_relative '../lib/player.rb'
 
 describe Player do
-  # sets name and color.
+  # initializes name and color.
+
+  describe '#make_disc' do
+    subject(:red_player) { described_class.new('Player 1', 'red')}
+
+    it 'makes a red disc object' do
+      red_disc = red_player.make_disc
+      expect(red_disc.color).to be('red')
+    end
+  end
 end
